67673f8371f0f0ae689cb13050a0f52ab6231056
Sudhakarann <sudhakaran852582@gmail.com>
2025-01-25 23:25:30 +0530

    requirement.txt file add

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,12 @@
 from app import app  # Import your Flask app instance
+import os
 
 if __name__ == "__main__":
+    # Enable template auto-reloading for development
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run()
+
+    # Get the PORT from environment variables, fallback to 5000 for local use
+    port = int(os.environ.get("PORT", 5000))
+
+    # Run the app with the proper host and port
+    app.run(host="0.0.0.0", port=port)
